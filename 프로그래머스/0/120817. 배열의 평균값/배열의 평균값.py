735a42cc864126dcfb83f426ea8e76fabc65bcd8
sojungoio <140520519+sojungoio@users.noreply.github.com>
2024-02-28 00:44:41 +0900

    [level 0] Title: 배열의 평균값, Time: 0.07 ms, Memory: 28.2 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -1,5 +1,9 @@
+import numpy
+
 def solution(numbers):
     answer = 0
-    for aver in numbers:
-        answer += aver / len(numbers)
+    # for aver in numbers:
+    #     answer += aver / len(numbers)
+    answer = numpy.mean(numbers)
     return answer
+    
