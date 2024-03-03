88e93e6f5de20cd03a5601881ca0888fa94e9163
sojungoio <140520519+sojungoio@users.noreply.github.com>
2024-03-03 19:31:47 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -4,20 +4,23 @@
 3. 해보자
 """
 
-def solution(numbers):
-    answer = 0
-    numbers.sort(reverse=True)
-    for i in numbers[0 : 1]:
-        for j in numbers[1 : 2]:
-            answer = i * j
-    return answer
+# def solution(numbers):
+#     answer = 0
+#     numbers.sort(reverse=True)
+#     for i in numbers[0 : 1]:
+#         for j in numbers[1 : 2]:
+#             answer = i * j
+#     return answer
 
 """
-중첩 for문을 이용하지 않고 할 수 있는 방법이 있을까 
+중첩 for문을 이용하지 않고 할 수 있는 방법이 있을꺼 같은데..
+"""
 
 def solution(numbers):
-    answer = 0
     numbers.sort(reverse=True)
-    for i in numbers[0 : 2]:
+    return numbers[0]*numbers[1]
 
+"""
+내림차순으로 정렬했는데.. 거기서 for문을 돌릴 이유가 없었네.. 
+다음부터는 그러지 말기..!!!
 """
