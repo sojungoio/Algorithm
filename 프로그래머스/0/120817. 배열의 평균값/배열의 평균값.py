35a7ed5b79a74c5adc613348d7c40ff7db18f10d
heohy <heohyeokjin950504@gmail.com>
2024-03-10 13:21:00 +0900

    20240310 배열의 평균값 첨삭.

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -1,9 +1,18 @@
-import numpy
 
+# import numpy
+
+# def solution(numbers):
+#     answer = 0
+#     # for aver in numbers:
+#     #     answer += aver / len(numbers)
+#     answer = numpy.mean(numbers)
+#     return answer
+
+
+"""
+heohy
+간단한 연산 함수를 작성하기 위해 numpy 를 import 하는 것은 자원 측면에서 비효율적 이므로,
+파이썬 기본 내장 라이브러리로 함수를 작성 하는 게 좋다.
+"""
 def solution(numbers):
-    answer = 0
-    # for aver in numbers:
-    #     answer += aver / len(numbers)
-    answer = numpy.mean(numbers)
-    return answer
-    
+    return sum(numbers) / len(numbers)
