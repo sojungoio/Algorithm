8a8766f30d2ab67de465d9ce8edbbd164cb4c3cf
sojungoio <140520519+sojungoio@users.noreply.github.com>
2024-06-23 20:03:37 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    answer = 0
+    
+    if a % 2 != 0 and b % 2 != 0:
+        answer = (a*a) + (b*b)
+    elif a % 2 == 0 and b % 2 == 0:
+        answer = abs(a-b)
+    else:
+        answer = 2 * (a+b)
+    return answer
